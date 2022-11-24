9fe788193f02d9659cbbe798539c9d0466dcfbfc
ByeonJuHwan <asdcz@naver.com>
2022-11-24 16:57:27 +0900

    65일차 댓글 달기 완->수정,삭제남음

--- spring03/src/main/resources/static/js/reply.js
+++ spring03/src/main/resources/static/js/reply.js
@@ -6,6 +6,7 @@
  
 window.addEventListener("DOMContentLoaded",()=>{
     // HTMLdml Document Object 들이 모두 로딩이 끝나 후에 코드들이 실행될 수 있도록 하기 위해서.
+    readAllReplies(); // 포스트 상세 페이지가 로딩된 후 댓글 목록 화면 출력.
     
     // btnReplyRegister 버튼을 찾고 이벤트 리스너를 등록.
     const btnReplyRegister = document.querySelector('#btnReplyRegister');
@@ -38,10 +39,53 @@ window.addEventListener("DOMContentLoaded",()=>{
         axios.post('/api/reply',reqParams) // Ajax Post 요청 보냄
             .then(response => { // 성공 응답(response)이 도착했을 때 실행할 콜백
                 console.log(response)
-                
+                alert('#'+response.data+'댓글 등록 성공'); 
+                clearInputs(); // 댓글 작성자, 내용에 작성된 문자열을 삭제.
+                readAllReplies(); // 댓글 목록을 다시 요청, 갱신.
             })
             .catch(error=>{ // 실패 응답(error)이 도착했을 때 실행할 콜백
                 console.log(error)
             });
     }
+
+    function clearInputs(){
+        document.querySelector('#writer').value = '';
+        document.querySelector('#replyText').value = '';
+    }
+    
+     function readAllReplies(){
+         const postId = document.querySelector('#id').value;
+         
+        axios
+        .get('/api/reply/all/'+postId)
+        .then(response=>{
+            updateReplyList(response.data)
+        })
+        .catch(error =>{console.log(error)});
+    }
+    
+    function updateReplyList(data){
+        // 댓글들의 배열(data)을 HTML 영역에 보일 수 있도록 html 코드 작성.
+        const divReplies = document.querySelector('#replies')
+        
+        let str = ''; // div 안에 들어갈 HTML 코드
+        for(let r of data){
+            str += '<div class="card my-2">'
+                + '<div class="card-header">'
+                + '<h5>'+ r.writer + '</h5>'
+                + '</div>'
+                + '<div class="card-body">'
+                + '<p>'+r.replyText+'</p>'
+                + '<p>작성시간 : '+r.createdTime+'</p>'
+                + '<p>수정시간 : '+r.modifiedTime+'</p>'
+                + '</div>'
+                + '<div class="card-footer">'
+                + `<button type="button" class="btnModifies btn btn-outline-primary" data-rid="${r.replyId}">수정</button>`
+                + '</div>'
+                + '</div>';
+        }
+        divReplies.innerHTML = str;
+        
+    }
+    
 });
